f565ca3df15666502fab04f530ae83e3774c03cf
yyyany <2332491@cegeprdl.ca>
2025-11-13 09:07:09 -0500

    Mise en place de l'environement et ajout de useFetch

--- App.tsx
+++ App.tsx
@@ -1,63 +1,4 @@
-/*
-import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleLogin = () => {
-    setSubmitted(true);
-  };
-
-  return (
-    <View style={styles.container}>
-      {!submitted ? (
-        <>
-          <Text style={styles.title}>Connexion</Text>
-          <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={setEmail}
-            accessibilityLabel="emailInput"   // 👈 Maestro s'en sert
-            style={styles.input}
-          />
-          <Button
-            title="Se connecter"
-            accessibilityLabel="submitButton" // 👈 Maestro s'en sert
-            onPress={handleLogin}
-          />
-        </>
-      ) : (
-        <Text accessibilityLabel="welcomeText">
-          Bienvenue {email}
-        </Text>
-      )}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  input: {
-    width: "80%",
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 20,
-  },
-});
-*/
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
@@ -71,7 +12,7 @@ export default function App() {
 
   const handleLogout = () => {
     setSubmitted(false);
-    setEmail(""); // Optionnel : réinitialiser l'email
+    setEmail(""); 
   };
 
   return (
---
+++ composables/useLocalStorage.ts
@@ -0,0 +1,18 @@
+const useLocalStorage = <T>(key: string) => {
+  const getItem = (): T | null => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  };
+
+  const setItem = (value: T): void => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  const removeItem = (): void => {
+    localStorage.removeItem(key);
+  };
+
+  return { getItem, setItem, removeItem };
+};
+
+export default useLocalStorage;
